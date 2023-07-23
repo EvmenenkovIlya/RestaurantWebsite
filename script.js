18ee7d932f6fd6072bca747e1855a0f2066f8942
Ilya <Evmenya@gmail.com>
2023-07-23 19:38:15 +0300

    It is for mobile too now

---
+++ script.js
@@ -0,0 +1,28 @@
+let count = 0;
+const text = document.getElementById("get-started").innerHTML;
+
+function getRandomColor() {
+    var letters = '0123456789ABCDEF';
+    var color = '#';
+    for (var i = 0; i < 6; i++) {
+      color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+  }
+
+function getStarted() {
+    let el = document.getElementById("get-started");
+    count += 1;
+    el.innerHTML = `${text} ${count}`;
+    el.style.backgroundColor = getRandomColor();
+}
+
+function showMenu() {
+  let el = document.querySelector("nav");
+  
+  if (el.classList.contains("unvisible")){
+    el.classList.remove("unvisible");
+  } else {
+    el.classList.add("unvisible")
+  }
+}
